234ac9a621bff7b949ead57014522ad9e6333dec
wildapes <luciferhellmaster@gmail.com>
2019-11-16 11:37:07 +0700

    Update movie.php

--- movie.php
+++ movie.php
@@ -14,21 +14,24 @@ if(isset($_GET['title'])){
 	$sql = "SELECT * FROM showing_tb inner join movie_tb on showing_tb.movie_id = movie_tb.movie_id inner join theater_tb on showing_tb.theater_id = theater_tb.theater_id";
 	$query = mysqli_query($connect, $sql) or die(mysqli_error($connect));
 }
+if(mysqli_num_rows($query) < 1){
+	return false;
+}else{
 
-foreach ($query as $list) {
-	$data[] = array(
-		'Theater' => $list['theater_name'],
-		'Judul Film' => $list['title'],
-		'Genre' => $list['genre'],
-		'Tanggal Rilis' => $list['release_date'],
-		'Durasi' => $list['duration'],
-		'Waktu Tayang'=> $list['show_time']
-	);
+	foreach ($query as $list) {
+		$data[] = array(
+			'Theater' => $list['theater_name'],
+			'Judul Film' => $list['title'],
+			'Genre' => $list['genre'],
+			'Tanggal Rilis' => $list['release_date'],
+			'Durasi' => $list['duration'],
+			'Waktu Tayang'=> $list['show_time']
+		);
+	}
+		$json = array(
+			'result' => 'success',
+			'data' => $data
+		);
+		echo json_encode($json);
 }
-
-$json = array(
-	'result' => 'success',
-	'data' => $data
-);
-echo json_encode($json);
 ?>
