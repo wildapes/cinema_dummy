c750533f501dd95d070d382ceac5e9e8ac3f8abc
wildapes <luciferhellmaster@gmail.com>
2019-11-16 11:11:17 +0700

    Create movie.php

---
+++ movie.php
@@ -0,0 +1,34 @@
+<?php 
+include "connect_db.php";
+
+
+if(isset($_GET['title'])){
+	$title = $_GET['title'];
+	$sql = "SELECT * FROM showing_tb inner join movie_tb on showing_tb.movie_id = movie_tb.movie_id inner join theater_tb on showing_tb.theater_id = theater_tb.theater_id where title='$title'";
+	$query = mysqli_query($connect, $sql) or die(mysqli_error($connect));
+}elseif(isset($_GET['theater'])){
+	$theater = $_GET['theater'];
+	$sql = "SELECT * FROM showing_tb inner join movie_tb on showing_tb.movie_id = movie_tb.movie_id inner join theater_tb on showing_tb.theater_id = theater_tb.theater_id where theater_tb.theater_id='$theater'";
+	$query = mysqli_query($connect, $sql) or die(mysqli_error($connect));
+}else{
+	$sql = "SELECT * FROM showing_tb inner join movie_tb on showing_tb.movie_id = movie_tb.movie_id inner join theater_tb on showing_tb.theater_id = theater_tb.theater_id";
+	$query = mysqli_query($connect, $sql) or die(mysqli_error($connect));
+}
+
+foreach ($query as $list) {
+	$data[] = array(
+		'Theater' => $list['theater_name'],
+		'Judul Film' => $list['title'],
+		'Genre' => $list['genre'],
+		'Tanggal Rilis' => $list['release_date'],
+		'Durasi' => $list['duration'],
+		'Waktu Tayang'=> $list['show_time']
+	);
+}
+
+$json = array(
+	'result' => 'success',
+	'data' => $data
+);
+echo json_encode($json);
+?>
